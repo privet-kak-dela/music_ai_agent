d3d69873d90dd53964af2ac3aea338270eb94dea
Amalia <115399488+privet-kak-dela@users.noreply.github.com>
2026-05-02 23:55:12 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print(123)
+from yandex_music import Client
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Client(os.environ.get('YANDEX_ACCESS_TOKEN')).init()
+
+print(client.users_likes_tracks()[0].fetch_track())
